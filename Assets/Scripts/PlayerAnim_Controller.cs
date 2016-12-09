4970f22532f660758fc91e4675ec4e21c286ea81
Unknown <david_force-fx@hotmail.com>
2016-12-09 18:20:04 +0100

    Animaciones main character
    Ahora el personaje se mueve por animaciones, pero cambia el control. Igual no se debería mover por animaciones para que quede como antes.

---
+++ Assets/Scripts/PlayerAnim_Controller.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnim_Controller : MonoBehaviour {
+
+	private Animator animator;
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator> ();
+		mainCamera = FindObjectOfType<Camera> ();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 currentPosition = (transform.position);
+
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+
+		print (v);
+
+		animator.SetFloat ("Speed Y", v);
+		animator.SetFloat ("Speed X", h);
+
+		//Rotacion
+
+		Ray cameraRay = mainCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayLenght;
+
+		if (groundPlane.Raycast (cameraRay, out rayLenght)) {
+			Vector3 pointToLook = cameraRay.GetPoint (rayLenght);
+			Debug.DrawLine (cameraRay.origin, pointToLook, Color.red);
+			Debug.DrawLine (currentPosition, pointToLook, Color.red);
+
+			transform.LookAt (new Vector3 (pointToLook.x, transform.position.y, pointToLook.z));
+		}
+
+	}
+}
