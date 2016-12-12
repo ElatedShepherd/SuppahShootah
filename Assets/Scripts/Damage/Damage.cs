5c7d638e6465f61ad0795e7df737ff83b6ed5922
JCG <jcgg92@gmail.com>
2016-12-12 22:37:00 +0100

    FIX

--- Assets/Scripts/Damage/Damage.cs
+++ Assets/Scripts/Damage/Damage.cs
@@ -2,31 +2,39 @@
 using System.Collections;
 
 public class Damage : MonoBehaviour {
+
 	
 	public float hitPoints; 
 	public bool isEnemy = false;
+	[Space(3)]
+	public LayerMask[] damageLayers;
 
-	public LayerMask proyectileLayer;
-
+	[Header("Referencias")]
 	public WaveController wc;
 
 
 
 	void Update () {
-				
+		
+
 		if (hitPoints <= 0){
 			
 			if (isEnemy)
 				wc.enemiesKilled++;
 
 			Destroy(gameObject);
-
 		}
 	}
 
 	void OnCollisionEnter (Collision col){
-		if (1<<col.gameObject.layer == proyectileLayer.value)
-		hitPoints --;
+		for (int i = 0; i < damageLayers.Length; i++) {
+			if (1 << col.gameObject.layer == damageLayers [i].value) {
+				if (col.gameObject.GetComponent<Shooter> () != null)
+					hitPoints -= col.gameObject.GetComponent<Shooter> ().currentWeapon.damage;
+				if (col.gameObject.GetComponent<AreaDamage> () != null)
+					hitPoints -= col.gameObject.GetComponent<AreaDamage> ().damage;
+			}
+		}
 	}
 
 }
