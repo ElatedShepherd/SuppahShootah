b0bd67106a742aef34a5517fbee834f054e13fa6
Unknown <david_force-fx@hotmail.com>
2016-12-12 19:05:24 +0100

    Cosas clase
    asd

---
+++ Assets/SetActiveBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetActiveBox : MonoBehaviour {
+
+	public GameObject Box;
+
+	void SetActive () {
+	
+		Box.SetActive (true);
+
+	}
+}
---
+++ Assets/SetInRange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetInRange : MonoBehaviour {
+
+	public Animator anim;
+
+	void OnTriggerEnter(Collider col) {
+
+		anim.SetBool ("inRange", true);
+
+	}
+
+	void SetInRangeOff() {
+
+		anim.SetBool ("inRange", false);
+
+	}
+
+}
