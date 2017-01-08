2dfc719357e4e0d18b828d6d40f510e3d911b9b0
Unknown <david_force-fx@hotmail.com>
2017-01-08 16:45:13 +0100

    Cosas
    mas cossas

---
+++ Assets/Scripts/Enemies/FreezePosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezePosition : MonoBehaviour {
+
+	public void FreezePositionY() {
+		
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+
+	}
+}
