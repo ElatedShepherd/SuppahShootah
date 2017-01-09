79acbe1058a66c05f4b7b18da0d5e5eaaf9e1de0
JCG <jcgg92@gmail.com>
2017-01-09 04:41:44 +0100

    G Launcher

---
+++ Assets/grenade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grenade : MonoBehaviour {
+	public float timer;
+	public GameObject trail;
+
+	void OnCollisionEnter (){
+		StartCoroutine(WaitAndDestroy(timer));
+	}
+
+	IEnumerator WaitAndDestroy (float s){
+		GetComponent<Rigidbody>().velocity = new Vector3 (0,0,0);
+		GetComponent<CapsuleCollider>().enabled = false;
+		GetComponent<MeshRenderer>().enabled = false;
+		trail.SetActive(false);
+		this.enabled = false;
+		yield return new WaitForSeconds(s);
+		Destroy(gameObject);
+	}
+}
