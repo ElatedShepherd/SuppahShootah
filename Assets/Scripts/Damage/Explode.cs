40b8bbda28eeb6cd981495d3e7a90aec2be10dcb
JCG <jcgg92@gmail.com>
2017-01-09 02:48:09 +0100

    Exploding Zombie
    too stronk

---
+++ Assets/Scripts/Damage/Explode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+	
+	public bool onDeath;
+	public bool onContact;
+	public bool onTimer;
+	public float timer;
+
+	[Space(3)]
+	public bool exploded;
+
+	public GameObject Aoe;
+
+	// Update is called once per frame
+	void Update () {
+		if(onDeath && !exploded) {
+			if(GetComponent<Damage>().hitPoints <= 0){
+				exploded = true;	
+				Aoe.GetComponent<AreaDamage>().Explode();
+			}
+		}
+	}
+}
